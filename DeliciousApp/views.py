914ee9f2c9b32c67f8fb83054dfb6c07de0cdcdb
R2a0n0i3 <160598123+R2a0n0i3@users.noreply.github.com>
2024-02-20 16:59:56 +0530

    Update views.py

--- DeliciousApp/views.py
+++ DeliciousApp/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, './DeliciousApp/index.html')
+    return render(request, './index.html')
 def login(request):
     return render(request, "DeliciousApp/login.html")
 # Create your views here.
